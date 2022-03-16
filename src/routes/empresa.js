1fffe7ba3415b9a83291e80e1633cf3fcdc1ec13
Dorian L.O <dalo1997@hotmail.es>
2022-03-16 01:30:49 -0500

    vistas-cliente Ajustes vista principal Cliente_Empresa Fixes #1

--- src/routes/empresa.js
+++ src/routes/empresa.js
@@ -1,38 +1,24 @@
 const express = require('express')
 const router = express.Router()
-
 const pool = require('../database')
 
-router.get('/add', (req, res) => {
-    // res.send("Hola desde Add Links")
-    res.render('empresa/addEmpresa')
+// Listar Categorías para realizar solicitudes
+router.get('/', async (req, res) => {
+    const empresa = await pool.query('SELECT * FROM empresa')
+    res.render('empresa/list', {empresa})
+})
+
+router.get('/fichaCliente', async (req, res) => {
+    const empresa = await pool.query('SELECT * FROM empresa')
+    res.render('empresa/addEmpresa', {empresa})
 })
 
 router.post('/add', async (req, res) => {
         let { nombre, apellido, email, telefono, fecha_nacimiento, pais, twitter, facebook, instagram, otra, es_propietario, socios, nombre_empresa, cantidad_socios, porcentaje_accionario, tiempo_fundacion, tiempo_experiencia, promedio_ingreso_anual, num_empleados, page_web, descripcion, etapa_actual, objetivo1, objetivo2, objetivo3, fortaleza1, fortaleza2, fortaleza3, problema1, problema2, problema3, motivo_consultoria } = req.body
-        const redes_sociales = {
-            twitter: twitter,
-            facebook: facebook,
-            instagram: instagram,
-            otra: otra
-        }
-        const objetivos = {
-            objetivo1: objetivo1,
-            objetivo2: objetivo2,
-            objetivo3: objetivo3,
-        }
-        const fortalezas = {
-            fortaleza1: fortaleza1,
-            fortaleza2: fortaleza2,
-            fortaleza3: fortaleza3,
-        }
-        const problemas = {
-            problema1: problema1,
-            problema2: problema2,
-            problema3: problema3,
-        }
-        // if (es_propietario == undefined) es_propietario = 'No'
-        // if (socios == undefined) socios = 'No'
+        const redes_sociales = { twitter, facebook, instagram, otra }
+        const objetivos = { objetivo1, objetivo2, objetivo3 }
+        const fortalezas = { fortaleza1, fortaleza2, fortaleza3 }
+        const problemas = { problema1, problema2, problema3 }
         es_propietario != undefined ? es_propietario : es_propietario = 'No'
         socios != undefined ? socios : 'No'
 
@@ -41,13 +27,22 @@ router.post('/add', async (req, res) => {
         }
         console.log(newFichaCliente)
         await pool.query('INSERT INTO empresa SET ?', [newFichaCliente])
-        res.send("Recibido desde Add Empresa")
+        res.redirect('/empresa')
     })
 
 // Listar info del formulario llenado
-router.get('/', async (req, res) =>{
+router.get('/', async (req, res) => {
     const empresa = await pool.query('SELECT * FROM empresa')
     console.log(empresa)
+    res.render('empresa/list', {empresa})
+})
+
+// Eliminar empresa
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params
+    await pool.query('SELECT * FROM empresa WHERE id = ?', [id])
+    // res.send('ELIMINADO')
+    res.redirect('/empresa')
 })
 
 module.exports = router
