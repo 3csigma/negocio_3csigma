07ded533b4a0cc470e5cabfd25e137dbe672301c
Dorian L. Orozco <dalo1997@hotmail.es>
2022-04-30 10:40:18 -0500

    Corrección Ruta ClavePrivada Docusign + const

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -9,6 +9,33 @@ const algorithm = 'aes-256-cbc'; //Using AES encryption
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
 const helpers = {}
+const clavePrivada = `-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAigXm2qUq1AmSHjR2gYK/9p+im8+veSwxddgbO+fTKA5gtU85
+olHr68WJsEmQwkVVahcs697h1LL+4WvDPNnTZKKk4eEk2ohAbls+BjO6vbL8gBSR
+tMh7chidqf4dQbN/Mvqz0Gh8fdGNIJau/v3FRGI+brVxamiGENpvdetYb8X3F0+C
+CsyJiTvQYdkjhZWigaKYY/RXrZYkjQqoZm92sl5xiXAy5+8pmTPsc2IVc2EmbYPC
+FPJY4DrH3EDAmnkGc2ekoaM5wHT0HbsYCaj7sFNY1MM8dfcP2x8ulGvMDNb5UmyA
+7X+aGyjKfmluK005UD8fcI9YCaseK5jgMXSDJQIDAQABAoIBAAR64CG/ZkUfNh67
+S1ki9IjdjXgh8q/HQqgDNFnzpOFyLXQgjbBIBt5p5vEj+iWHj+EcmeRQDxF3jiuk
+hU241SSnAfhbPtGhWGacmSGS1UTOmEDTKhfbU3cCQIj9OKCZYiHIjGks/DBTSrZT
+AleJfZSdlAVXw1hxNDup82jwE0q2ASK9dlK6a8F2cHb0Aio+3qsy16rCOp1Un8+Q
+rtSjqIn+KdaZtVLY2FYMqAR2jZL3UnxkmAgIkDRoyhFvwIqC3kOMFuqYNt+A4sXQ
+faM1M3D7g4a4jg6BDjt9bF4SUGU7vaPwr+Q19GseUcrB07EGiF6pXSdlNOqjUn7E
+jgPcdxECgYEAvPMAscJTYRCSdsaZKj7SQdrV2SzVgpGXbONagNUclpYYfzKFqok/
+5ZlaZEQ39OpUgO+rIrxhDYt+VMEl/N9765nT/xlKtpceBYmoj0mxmytPGQN1b9De
+CbkFk9F/S9edf4qSxOTLZuTMYOdW2d3LAX9apwWhFSJv1MwgImf34OkCgYEAuwCJ
+T5pC7WVW/+tTXpXIZj1IELp4+BRkof6/Qwcbzf0oJatbxPuYLib64qTGvBRGhWTJ
+n28NV2uen7oM9A9wXv4egOiHX53Cc/LHNDErn47zUpJhH9bdOhdPdX990lRGPSgP
+vPmEFK/78NcdxNzUelxV+DFuW2dRozm++vVUSt0CgYAsl8EjMrSQ5ONcKtS9FqRy
+rF3p6l3dedTd+u5CF4mutG/FnTvUmp285ythHISERcV5xJaEFSdmlfRTtl3rxyas
+3Noei26JlzKbpfhlEMRc1aTOR3Ww1P+CFeuODlRXpuRh1Gd9Fm42s2rUt+RUeLbP
+fKn9XyO8JCt2clJnLFhg+QKBgC661pVOzjS2o0P2kXHMajSqUazkfEA6EF+u4iqk
+xxz1iyfGp3sVCBVYZk9mr8bmJi1FX2D5BlN3MD9n5b7DcOg8BnFeKh4JtUL2QzNW
+qjkIJuPWcnDbZ5MRqiiJgG6j4p17ulsV2xy0UXgAuY6tRGeDVXRdHd+3dN4eGwxp
+a/P9AoGBAJGHvzEVku3y4SAEtaBacYrLXq81+1741+TnvkQrg026GQufpOLBtq3t
+ZxJ8U9ZbPhV4U+kWJ1qeHRUmgmmFt08d0ZU08/EywqwkgwrSUn1SUBf+eMIClK6r
+55If65RnCnlHvCOmujwWZOXkql1ZHXPzFcmRc90l7ucJLQzC7oK7
+-----END RSA PRIVATE KEY-----`
 
 // Encriptar clave
 helpers.encryptPass = async (password) => {
@@ -28,11 +55,11 @@ helpers.matchPass = async (password, passDB) => {
 // Generar Token de Autenticación en API Docusing
 helpers.authToken = async () => {
     // Leyendo Clave Privada RSA emitida por Docusing
-    const privateKey = fs.readFileSync('src/config/private.key');
+    // const privateKey = fs.readFileSync('src/config/private.key');
     // const privateKey = fs.readFileSync(dsConfig.privateKeyRSA);
     try {
         //Generando Token de Autenticación para DocuSign
-        dsConfig.authToken = jwt.sign(dsConfig.dsPayload, privateKey, { algorithm: 'RS256' })
+        dsConfig.authToken = jwt.sign(dsConfig.dsPayload, clavePrivada, { algorithm: 'RS256' })
     } catch (error) {
         console.log(error)
     }
