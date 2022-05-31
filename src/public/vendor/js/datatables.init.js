2afaac168d8734df83faa59f4ca7cfdd7ec80850
Dorian L. Orozco <dalo1997@hotmail.es>
2022-05-30 22:39:57 -0500

    Dashboard Admin Part2 Implementando gráfica Fixes #23

--- src/public/vendor/js/datatables.init.js
+++ src/public/vendor/js/datatables.init.js
@@ -1,7 +1,12 @@
-(function($) {
+(function ($) {
     "use strict"
-    
-    $('#consultoresTabla').DataTable();
-    $('#empresasTabla').DataTable();
-      
+
+    $('#consultoresTabla').DataTable({
+        "language": { "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json" },
+    });
+
+    $('#empresasTabla').DataTable({
+        "language": { "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json" },
+    });
+
 })(jQuery);
