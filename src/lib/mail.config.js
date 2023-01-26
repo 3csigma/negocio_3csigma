6943f22722339b3e22cebc9b06d1af0c1407bc33
Dorian L.O <dalo1997@hotmail.es>
2023-01-26 13:48:04 -0500

    Fix mail config nodemailer v8

--- src/lib/mail.config.js
+++ src/lib/mail.config.js
@@ -11,14 +11,16 @@ const mail = {
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-	tls: {rejectUnauthorized: false},
 	host: "smtp.office365.com",
 	port: 587,
 	secure: false, // true for 465, false for other ports
-	// service: "Outlook365",
 	auth: {
 		user: mail.user,
 		pass: mail.pass,
+	},
+	tls: {
+		// do not fail on invalid certs
+		rejectUnauthorized: false,
 	},
 });
 
