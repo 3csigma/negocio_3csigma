06eaa967accd619d7c0c13f619703b7065015bfb
Dorian L.O <dalo1997@hotmail.es>
2023-01-26 17:31:51 -0500

    Fix mail config nodemailer versión base (v16)

--- src/lib/mail.config.js
+++ src/lib/mail.config.js
@@ -2,18 +2,18 @@ const nodemailer = require("nodemailer");
 const { my_domain } = require("../keys").config
 
 const mail = {
-	// user: 'noreply@3csigma.com',
-	// pass: '&gxq6DOCYk$I'
-	user: 'hello@3csigma.com',
-	pass: 'OAjN-3jQd*y-t#eq',
+	user: 'noreply@3csigma.com',
+	pass: '&gxq6DOCYk$I'
+	// user: 'hello@3csigma.com',
+	// pass: 'OAjN-3jQd*y-t#eq',
 	// pass: 'lrrbbvcvrhmvpzmf',
 }
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-	host: "smtpout.secureserver.net",
-	secure: false,
-	port: 25,
+	host: "3csigma.com",
+	port: 465,
+	secure: true,
 	auth: {
 		user: mail.user,
 		pass: mail.pass,
