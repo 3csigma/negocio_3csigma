bcd6946a92d82ac0814dce4e106cb989eabc8bda
Dorian L.O <dalo1997@hotmail.es>
2023-01-26 13:52:42 -0500

    Fix mail config nodemailer v10

--- src/lib/mail.config.js
+++ src/lib/mail.config.js
@@ -2,26 +2,26 @@ const nodemailer = require("nodemailer");
 const { my_domain } = require("../keys").config
 
 const mail = {
-	// user: 'noreply@3csigma.com',
-	// pass: '&gxq6DOCYk$I'
-	user: 'hello@3csigma.com',
+	user: 'noreply@3csigma.com',
+	pass: '&gxq6DOCYk$I'
+	// user: 'hello@3csigma.com',
 	// pass: 'OAjN-3jQd*y-t#eq',
-	pass: 'lrrbbvcvrhmvpzmf',
+	// pass: 'lrrbbvcvrhmvpzmf',
 }
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-	host: "smtp.office365.com",
-	port: 587,
-	secure: false, // true for 465, false for other ports
+	host: "3csigma.com",
+	port: 465,
+	secure: true, // true for 465, false for other ports
 	auth: {
 		user: mail.user,
 		pass: mail.pass,
 	},
-	tls: {
-		// do not fail on invalid certs
-		rejectUnauthorized: false,
-	},
+	// tls: {
+	// 	// do not fail on invalid certs
+	// 	rejectUnauthorized: false,
+	// },
 });
 
 /** Enviar el email */
