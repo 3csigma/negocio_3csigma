da9795aca206ad3ca29a97ef8f93c37c8d0c8ffe
Dorian L.O <dalo1997@hotmail.es>
2023-04-04 19:03:29 -0500

    Hot Fix General (Versión 6.5) - Campo email editable sección aprobar consultor - Add Eliminar Consultores - Add Eliminar Empresas - Add Validaciones al eliminar Consultores y Empresas - Add nuevas rutas para las funciones de eliminar

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const rutasEmpresa = ['logout', 'perfil', 'updateprofile', 'actualizarfotoperfil
 
 const rutasConsultor = ['logout', 'perfil', 'updateprofile', 'actualizarfotoperfil', 'comentariotareas', 'empresas', 'empresas-asignadas', 'enviar-propuesta-empresa', 'analisis-dimension-producto', 'analisis-dimension-administracion', 'analisis-dimension-operaciones', 'analisis-dimension-marketing', 'agregartarea', 'editartarea', 'actualizartarea', 'eliminartarea', 'nuevorendimiento', 'cuestionario-diagnostico', 'diagnostico-proyecto', 'guardarinforme', 'conclusiones', 'guardar-archivos-empresarial', 'website-empresarial', 'finalizaretapa', 'solicitar-archivos-empresa', 'eliminar-archivos-empresa']
 
-const rutasAdmin = ['logout', 'perfil', 'updateprofile', 'actualizarfotoperfil', 'consultores', 'actualizarconsultor', 'bloquearconsultor', 'empresas', 'actualizarempresa', 'bloquearempresa', 'pagomanual-diagnostico', 'pagomanual-empresas', 'cancelarsub']
+const rutasAdmin = ['logout', 'perfil', 'updateprofile', 'actualizarfotoperfil', 'consultores', 'actualizarconsultor', 'bloquearconsultor', 'empresas', 'actualizarempresa', 'bloquearempresa', 'pagomanual-diagnostico', 'pagomanual-empresas', 'cancelarsub', 'eliminarempresa', 'eliminarconsultor']
 
 const privateKey_DKIM = `-----BEGIN RSA PRIVATE KEY-----
 MIIEowIBAAKCAQEAxnOodBzxqzPuLrbLtK34c/RwxGDOUcb9R1505XsocKSxuFZS
