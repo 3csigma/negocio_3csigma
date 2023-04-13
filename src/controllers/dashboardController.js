514e7481447a219905cbee1355d7ace8b4cd2f06
jcarlosabc <jeancatovio23@gmail.com>
2023-04-13 17:53:15 -0500

    Add borrar consultores

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -268,6 +268,26 @@ dashboardController.bloquearConsultor = async (req, res) => {
     }
 }
 
+dashboardController.eliminarConsultor = async (req, res) => {
+    const { id } = req.body
+    let result = false;
+    let datos = await consultarDatos('consultores_asignados')
+    datos = datos.find(x => x.consultor == id)
+    console.log("filtro" , datos);
+    if (datos) {
+       console.log(" aqui no borramos");
+    }else if(datos == undefined) {
+        console.log("aqui borramos");
+        datos = await consultarDatos('consultores')
+        datos = datos.find(x => x.id_consultores == id)
+        
+        await pool.query('DELETE FROM users WHERE codigo = ?', [datos.codigo])
+        await pool.query('DELETE FROM consultores WHERE codigo = ?', [datos.codigo])
+         result = true;
+    }
+    res.send(result)
+}
+
 // EMPRESAS
 dashboardController.mostrarEmpresas = async (req, res) => {
     let empresas = await pool.query('SELECT e.*, u.codigo, u.estadoEmail, u.estadoAdm, f.telefono, f.id_empresa, p.*, a.id_empresa, a.estadoAcuerdo FROM empresas e LEFT OUTER JOIN ficha_cliente f ON f.id_empresa = e.id_empresas LEFT OUTER JOIN pagos p ON p.id_empresa = e.id_empresas LEFT OUTER JOIN acuerdo_confidencial a ON a.id_empresa = e.id_empresas INNER JOIN users u ON u.codigo = e.codigo AND rol = "Empresa"')
